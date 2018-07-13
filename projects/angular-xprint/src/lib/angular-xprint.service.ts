7eddde0f7dffccad697f2c522448af6d61ed347c
Spero AMEY <ameyspero@gmail.com>
2018-07-13 15:06:08 +0100

    improve printing no canvas section

--- projects/angular-xprint/src/lib/angular-xprint.service.ts
+++ projects/angular-xprint/src/lib/angular-xprint.service.ts
@@ -8,47 +8,53 @@ export class AngularXprintService {
   constructor() { }
 
   __printDiv(printArea) {
-    
-/*     window.frames["print_frame"].document.write('<link rel="stylesheet" type="text/css" href="assets/state.css"/>');
- */      let table=printArea;
-     /*  window.frames["print_frame"].document.write(table) */
-     /*  window.frames["print_frame"].focus(); */
+      let _frame = this._cf();
+      document.querySelector('body').appendChild(_frame);
+      var link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'assets/state.css';
+      window.frames["print_frame"].document.head.appendChild(link);
+      var body = document.createElement('body');
+      let table=printArea;
+      /*  console.log( window['print_frame'].document); */
+      window.frames["print_frame"].document.body.innerHTML =table;
+      window.frames["print_frame"].focus();
+  }
+
+  _cf(){
+    let _frame = document.createElement('iframe');
+    _frame.name = "print_frame";
+    _frame.src = "about:blank";
+    _frame.width="0";
+    _frame.height="0";
+    return _frame;
   }
 
 
-  printAsPdf2(printArea,printCanvas?){
-          if (printCanvas.length > 0) {
-             var i;
-             for (i = 0; i < printCanvas.length; i++) {
-                (()=>{
-                  console.log(i);
-                  
-                  var imageElement= printCanvas[i].parentNode;
-                  var myImage = printCanvas[i].toDataURL("image/png");
-                  var image = new Image();
-                  console.log("mon url ",myImage);
-                  image.src = myImage;
-                  var div = document.createElement("div");
-                  imageElement.insertBefore(div, printCanvas[i]);
-                  printCanvas[i].parentNode.removeChild(printCanvas[i]);
-                  div.appendChild(image);
+  printAsPdf(printArea,printCanvas?){
+    if (printCanvas.length > 0) {
+          for (let i = 0; i < printCanvas.length; i++) {
+            (()=>{
+              var imageElement= printCanvas[i].parentNode;
+              var myImage = printCanvas[i].toDataURL("image/png");
+              var image = new Image();
+              image.src = myImage;
+              var div = document.createElement("div");
+              imageElement.insertBefore(div, printCanvas[i]);
+              printCanvas[i].parentNode.removeChild(printCanvas[i]);
+              div.appendChild(image);
                 
-                  if(i+1 == printCanvas.length){
-                    let _frame = document.createElement('iframe');
-                    _frame.name = "print_frame";
-                    _frame.src = "about:blank";
-                    _frame.width="0";
-                    _frame.height="0";
-
-                    imageElement.parentNode.insertBefore(_frame, imageElement.nextSibling);
-                    console.log(_frame);
-                    
-                    this.__printDiv(printArea);
-                  } 
-               })();
-             }
-          }else{
-            this.__printDiv(printArea);
-        }
+              if(i+1 == printCanvas.length){
+                
+               /*  let _frame = this._cf();
+                imageElement.parentNode.insertBefore(_frame, imageElement.nextSibling);
+ */                this.__printDiv(printArea);
+              } 
+            })();
+          }
+    }else{
+     
+      this.__printDiv(printArea);
+    }
   }
 }
--- projects/angular-xprint/src/lib/section.directive.ts
+++ projects/angular-xprint/src/lib/section.directive.ts
@@ -6,27 +6,25 @@ import { AngularXprintService} from './angular-xprint.service';
   
 })
 export class SectionDirective implements OnInit{
-  
    public ngOnInit(): void {
         let printArea=this.printZone.nativeElement.innerHTML;
         let printCanvas=this.printZone.nativeElement.querySelectorAll('canvas');
-        console.log(printCanvas.length);
         this.fc(printCanvas.length,printArea,printCanvas);
-      }
+    }
 
-      fc(l,printArea,printCanvas){
+    fc(l,printArea,printCanvas){
         if(l>0){
-          console.log(printCanvas);
-          this._stateService.printAsPdf2(printArea,printCanvas);
+         /*  console.log(printCanvas); */
+          this._stateService.printAsPdf(printArea,printCanvas);
           console.log("canvas");
         }else{
-          /* this._stateService.printAsPdf2(printArea); */
-          console.log(printArea);
+          this._stateService.printAsPdf(printArea,printCanvas);
+         /*  console.log(printArea); */
           console.log("no canvas");
         }
       }
 
-       constructor(
+      constructor(
           private printZone: ElementRef,
           private _changeDetectorRef:ChangeDetectorRef,
           private _stateService: AngularXprintService
